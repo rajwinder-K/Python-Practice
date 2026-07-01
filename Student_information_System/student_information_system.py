b9976f80131baa4ed2f4797dff9b6c19a9907f52
Rajwinder Kaur <rajwinder4519@gmail.com>
2026-07-01 08:37:06 +0530

    Implement student information input and display
    Added functionality to input and display student information.

--- Student_information_System/student_information_system.py
+++ Student_information_System/student_information_system.py
@@ -1,1 +1,16 @@
+name = input("Name: ")
+roll_number = int(input("Roll Number:"))
+age = int(input("Age: "))
+branch = input("Branch: ")
+semester = input("Semester: ")
+cgpa = float(input("CGPA: "))
+
+print("\n------ Student Card ------")
+
+print(f"Name         : {name}")
+print(f"roll_number  : {roll_number}")
+print(f"age          : {age}")
+print(f"branch       : {branch}")
+print(f"semester     : {semester}")
+print(f"cgpa         : {cgpa}")
 
