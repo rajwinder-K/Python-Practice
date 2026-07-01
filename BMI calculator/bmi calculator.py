022972c6392b5e1d1d996afef03399c0a4f062e4
Rajwinder Kaur <rajwinder4519@gmail.com>
2026-07-01 08:54:19 +0530

    Add BMI calculation code

--- BMI calculator/bmi calculator.py
+++ BMI calculator/bmi calculator.py
@@ -1,1 +1,8 @@
+weight = float(input("Weight(kg): "))
+height = float(input("Height(m): "))
 
+BMI = weight / (height ** 2)
+
+print(f"Weight :{weight}")
+print(f"Height : {height}")
+print(f"BMI    : {round(BMI,2)}")
